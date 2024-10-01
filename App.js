67a40227bc6fdf5642bd6c34ad062d5a3fa70f6e
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-10-01 18:22:05 -0300

    .

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [contador, setContador] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Contador de Clientes</Text>
+
+      <View style={styles.hStack}>
+        <Button style={styles.button} title="-1" onPress={diminuirContador}/>
+        <Text>{contador}</Text>
+        <Button style={styles.button} title="+1" onPress={aumentarContador}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
+
+
+  function aumentarContador () {
+    setContador(contador + 1)
+  }
+
+  function diminuirContador () {
+    setContador(contador - 1)
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +35,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: 'brown',
+    marginBottom: 100
+  },
+
+  hStack: {
+    flexDirection: 'row',
+    justifyContent: 'space-between', 
+    width: '70%', 
+  }
 });
